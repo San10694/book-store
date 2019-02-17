daf34096b865caa916ea54d9d46cffa1fe017044
Sandeep1061994 <sandeep.tiwari@groww.in>
2019-02-17 11:16:15 +0530

    drawer and tab layout fixes

--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -3,6 +3,11 @@ import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
 import { connect } from "react-redux";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { getRestaurantList } from "../Redux/ListRedux";
+import { MenuIcon } from '../Components/MenuIcon';
+import {
+  DrawerActions
+} from "react-navigation";
+import { Colors } from "../Themes";
 
 
 const myButton = (
@@ -19,26 +24,41 @@ class HomeScreen extends Component {
     super(props);
   }
 
+
+  static navigationOptions = ({ navigation }) => ({
+    header: null,
+  });
+
   componentDidMount() {
     //this.props.getRestaurantList();
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Home!</Text>
-        <Text style={styles.instructions}>Start Buying Books form this page</Text>
-
+      <View style={{ flex: 1, backgroundColor: Colors.white }}>
         <TouchableOpacity
-          onPress={() => this.props.navigation.navigate("ProfileScreen")}
+          onPress={() => {
+            this.props.navigation.dispatch(DrawerActions.toggleDrawer());
+          }}
         >
-          <Icon.Button
-            name="facebook"
-            backgroundColor="#3b5998"
-            onPress={this.loginWithFacebook}
-          >
-            Login with Facebook
-         </Icon.Button>
+          <MenuIcon navigation={this.props.navigation} />
         </TouchableOpacity>
+
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Welcome to Home!</Text>
+          <Text style={styles.instructions}>Start Buying Books form this page</Text>
+
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("ProfileScreen")}
+          >
+            <Icon.Button
+              name="facebook"
+              backgroundColor="#3b5998"
+              onPress={this.loginWithFacebook}
+            >
+              Login with Facebook
+         </Icon.Button>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -68,7 +88,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: Colors.white
   },
   welcome: {
     fontSize: 20,
@@ -77,7 +97,7 @@ const styles = StyleSheet.create({
   },
   instructions: {
     textAlign: "center",
-    color: "#333333",
+    color: Colors.Text,
     marginBottom: 5
   }
 });
