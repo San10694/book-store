f23ba9749eb43170fc6ec23fbc0f48e1056e539a
Sandeep1061994 <sandeep.tiwari@groww.in>
2019-03-08 22:32:32 +0530

    android changes

--- android/app/src/main/java/com/bookstore/MainActivity.java
+++ android/app/src/main/java/com/bookstore/MainActivity.java
@@ -1,23 +1,23 @@
 package com.bookstore;
 
 import com.facebook.react.ReactActivity;
-import com.reactnativecomponent.splashscreen.RCTSplashScreen; 
 
 public class MainActivity extends ReactActivity {
 
     /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
+     * Returns the name of the main component registered from JavaScript. This is
+     * used to schedule rendering of the component.
      */
     @Override
     protected String getMainComponentName() {
         return "BookStore";
     }
 
-//     @Override
-// protected void onCreate(Bundle savedInstanceState) {
-//     RCTSplashScreen.openSplashScreen(this);   //open splashscreen
-//     //RCTSplashScreen.openSplashScreen(this, true, ImageView.ScaleType.FIT_XY);   //open splashscreen fullscreen
-//     super.onCreate(savedInstanceState);
-// }
+    // @Override
+    // protected void onCreate(Bundle savedInstanceState) {
+    // RCTSplashScreen.openSplashScreen(this); //open splashscreen
+    // //RCTSplashScreen.openSplashScreen(this, true, ImageView.ScaleType.FIT_XY);
+    // //open splashscreen fullscreen
+    // super.onCreate(savedInstanceState);
+    // }
 }
